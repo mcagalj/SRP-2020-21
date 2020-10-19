5602ee329e4fae62c92ab966dce6237beb29d424
mcagalj <mario.cagalj@fesb.hr>
2020-10-19 13:06:04 +0200

    Add jwt

--- postgres-express-node/index.js
+++ postgres-express-node/index.js
@@ -8,6 +8,7 @@ const {
 const crypto = require("crypto");
 const fs = require("fs");
 const { ecb: cipher } = require("./services/ciphers");
+const jwt = require("jsonwebtoken");
 
 async function assertDatabaseConnectionOk() {
   console.log(`Checking database connection...`);
@@ -54,97 +55,31 @@ function printMedicalTests({ username = null, tests }) {
 }
 
 async function init() {
-  // await assertDatabaseConnectionOk();
-  // let user = await getUserByUsername("john");
-  // console.log(user);
-  // user = await getUserByUsername("mirta");
-  // console.log(user);
-  // let tests = await getMedicalTests({
-  //   attributes: ["result", "name"],
-  //   include: { model: User, attributes: ["username"] },
-  // });
-  // printMedicalTests({ tests });
-  // let username = "mirta";
-  // tests = await getMedicalTestsForUser(username);
-  // printMedicalTests({ username, tests });
-  // try {
-  //   let message = encrypt({
-  //     key: Buffer.from(process.env.DB_FIELD_ENC_KEY, "base64"),
-  //     plaintext: "test",
-  //   });
-  //   let decryptedMessage = decrypt({
-  //     key: Buffer.from(process.env.DB_FIELD_ENC_KEY, "base64"),
-  //     message,
-  //   });
-  //   console.table({
-  //     plaintext: "test",
-  //     encryptedMessage: message,
-  //     decryptedMessage,
-  //   });
-  // } catch (error) {
-  //   console.log(error.message);
-  // }
-  // const { id } = await getUserByUsername("john", { attributes: ["id"] });
-  // if (id) {
-  //   let newTest = MedicalTest.build({
-  //     UserId: id,
-  //     name: "HIV",
-  //     result: `negative`,
-  //     // result: JSON.stringify({ result: "negative", user: "john", id }),
-  //   });
-  //   newTest = await newTest.save();
-  //   console.log(newTest.toJSON());
-  // }
-  // tests = await getMedicalTestsForUser("john");
-  // printMedicalTests({ username: "john", tests });
-
   /**
    * * Message authentication (hash and MAC crypto functions)
    */
   try {
+    // * ========================
+    // *  Generate token
+    // * ------------------------
     const secret = "my supper secret";
-    let message = "Authenticate this message.";
+    let message = { message: "Authenticate this message." };
 
-    let hmac = crypto.createHmac("sha256", secret); // ! Message authentication code
-    let authTag = hmac.update(message).digest(); // ! Message digest/authentication code
-    console.table([
-      { message, "message digest/authentication tag": authTag.toString("hex") },
-    ]);
+    let token = jwt.sign(message, secret);
+    let decodedToken = jwt.decode(token);
+    console.log(token);
+    console.log(decodedToken); // * Return only payload
 
-    // * ===============================
-    // * Example: Authenticating a file
-    // *--------------------------------
-    const CREATE_TAG = false;
+    token = jwt.sign(message, secret, { expiresIn: "1h" });
+    decodedToken = jwt.decode(token, { complete: true }); // * Include all parts
+    console.log(token);
+    console.log(decodedToken);
 
-    if (CREATE_TAG) {
-      // * Authenticate the file
-
-      const input = fs.createReadStream("test.txt");
-      const output = fs.createWriteStream("test.tag");
-      const hmac = crypto.createHmac("sha256", secret);
-
-      hmac.update("text.txt"); // ! Protecting the file name
-      input
-        .pipe(hmac) // ! Protecting the file content
-        .pipe(output)
-        .on("finish", () => output.end());
-    } else {
-      // * Verify the file authenticity
-
-      const input = fs.createReadStream("test.txt");
-      const tag = fs.readFileSync("test.tag");
-      const hmac = crypto.createHmac("sha256", secret); // ! Message authentication code
-
-      hmac.update("text.txt");
-      input.pipe(hmac).on("finish", () => {
-        const isMessageAuthentic = crypto.timingSafeEqual(hmac.read(), tag); // ! Compare the auth tags
-        console.log(
-          `The file "test.txt" ${
-            isMessageAuthentic ? "IS" : "IS NOT"
-          } authentic.`
-        );
-      });
-    }
+    // * ========================
+    // *  Verify token
+    // * ------------------------
+    decodedValidatedToken = await verifyJwt({ secret, token });
+    console.log("Validated token:", decodedValidatedToken);
   } catch (err) {
     console.log(err.message);
   }
@@ -152,11 +87,17 @@ async function init() {
 
 init();
 
-//--------------------------------
-// Helper/utility functions
-//--------------------------------
-function increment(buffer) {
-  for (let i = buffer.length - 1; i >= 0; i--) {
-    if (buffer[i]++ !== 255) break;
-  }
+/**
+ * Promisfied JWT verification
+ * @param {*} token
+ */
+function verifyJwt({ token, secret }) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, secret, (error, decoded) => {
+      if (error) {
+        return reject({ error: error.message });
+      }
+      resolve(decoded);
+    });
+  });
 }
